34d414aec6e15773a938201490b4014ab4d73270
caballeroGISH <alexandr.lavrinenko@gmail.com>
2019-05-22 23:27:37 +0300

    IoC - less 2

---
+++ src/main/java/ru/alishev/springcourse/TestSpring.java
@@ -0,0 +1,16 @@
+package ru.alishev.springcourse;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Music music = context.getBean("musicBean", Music.class);
+
+        MusicPlayer musicPlayer = new MusicPlayer(music);
+        // System.out.println(music.getName()); -- Only lesson 1
+        musicPlayer.playMusic();
+        context.close();
+    }
+}
