68ad49da2671a1f52d3941020a6d8395f4564219
caballeroGISH <alexandr.lavrinenko@gmail.com>
2019-05-23 21:55:20 +0300

    add lesson 6 (Dependency Injection (DI) - part 2)

--- src/main/java/ru/alishev/springcourse/MusicPlayer.java
+++ src/main/java/ru/alishev/springcourse/MusicPlayer.java
@@ -4,9 +4,35 @@ public class MusicPlayer {
 
     private Music music;
 
+    // Урок 6: Внедрение зависимостей. Dependency Injection (DI)
+    private String name;
+    private int volume;
+
     //IoC - Inversion of Control
     public MusicPlayer(Music music) {
         this.music = music;
+    }
+
+    // Урок 6: Внедрение зависимостей. Dependency Injection (DI) - Конструктор без аргументов
+    public MusicPlayer() {
+
+    }
+
+    // Урок 6: Внедрение зависимостей. Dependency Injection (DI)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
     }
 
     // Урок 6: Внедрение зависимостей. Dependency Injection (DI)
--- src/main/java/ru/alishev/springcourse/TestSpring.java
+++ src/main/java/ru/alishev/springcourse/TestSpring.java
@@ -7,14 +7,18 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
+        // System.out.println(music.getName());                     // Only lesson 1
+
         // Music music = context.getBean("musicBean", Music.class); // For lesson 4
         // MusicPlayer musicPlayer = new MusicPlayer(music);        // For lesson 4
-
-        // System.out.println(music.getName());                     // Only lesson 1
 
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);    // Lesson 5 (DI)
 
         musicPlayer.playMusic();
+
+        // Урок 6: Внедрение зависимостей. Dependency Injection (DI). Часть 2
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
 
         context.close();    // !!!
     }
