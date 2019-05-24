6a92b7624d52395e0a99b0086405efc459c4cd43
caballeroGISH <alexandr.lavrinenko@gmail.com>
2019-05-24 22:04:36 +0300

    HW after lesson 6 - List (codemode)

--- src/main/java/ru/alishev/springcourse/MusicPlayer.java
+++ src/main/java/ru/alishev/springcourse/MusicPlayer.java
@@ -1,18 +1,25 @@
 package ru.alishev.springcourse;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
 
-    private Music music;
+    private List<Music> musicList = new ArrayList<>();
+        // Домашнее задание после урока 6 - модификация кода
+    // private Music music;
 
     // Урок 6: Внедрение зависимостей. Dependency Injection (DI)
     private String name;
     private int volume;
 
     //IoC - Inversion of Control
+        // Домашнее задание после урока 6 - модификация кода
+    /*
     public MusicPlayer(Music music) {
         this.music = music;
     }
-
+    */
     // Урок 6: Внедрение зависимостей. Dependency Injection (DI) - Конструктор без аргументов
     public MusicPlayer() {
 
@@ -34,13 +41,28 @@ public class MusicPlayer {
     public void setVolume(int volume) {
         this.volume = volume;
     }
+        // Домашнее задание после урока 6 - модификация кода
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
+    }
+
+    public void playMusicList() {
+        for (Music music: musicList) {
+            System.out.println("Playing: " + music.getSong());
+        }
+    }
 
     // Урок 6: Внедрение зависимостей. Dependency Injection (DI)
-    public void setMusic(Music music) {
-        this.music = music;
-    }
+        // Домашнее задание после урока 6 - модификация кода
 
+    /* public void setMusic(Music music) {
+        this.music = music;
+    }*/
+
+        // Домашнее задание после урока 6 - модификация кода
+    /*
     public void playMusic() {
+
         System.out.println("Playing: " + music.getSong());
-    }
+    }*/
 }
--- src/main/java/ru/alishev/springcourse/TestSpring.java
+++ src/main/java/ru/alishev/springcourse/TestSpring.java
@@ -13,8 +13,10 @@ public class TestSpring {
         // MusicPlayer musicPlayer = new MusicPlayer(music);        // For lesson 4
 
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);    // Lesson 5 (DI)
+            // Домашнее задание после урока 6 - модификация кода
+        //musicPlayer.playMusic();
+        musicPlayer.playMusicList();
 
-        musicPlayer.playMusic();
 
         // Урок 6: Внедрение зависимостей. Dependency Injection (DI). Часть 2
         System.out.println(musicPlayer.getName());
